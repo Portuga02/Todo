0306643c7e46a6c3f8e65c716ea79497f07fe215
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2021-10-28 17:09:27 -0300

    configurando App.vue

---
+++ src/components/Todo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="tile flex-centered">
+    <div class="tile-icon">
+      <div>
+        <i class="icon icon-time"></i>
+      </div>
+    </div>
+    <dir class="tile-content">
+      <div class="tile-subtitle">{{ todo.description }}</div>
+    </dir>
+    <div class="tile-action">
+      <button @click="$emit('toggle', todo)" class="btn btn-link">
+        Concluido
+      </button>
+      <button class="btn btn-link">
+        <span class="text-error">Remover </span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: { type: Object, required: true },
+  },
+};
+</script>
+
+<style scoped>
+.checked {
+  text-decoration: line-through;
+  color: lightgray;
+}
+</style>
